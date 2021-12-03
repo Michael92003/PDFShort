247cfde91d293f4b15368254ffce3100df18ecbf
Michael92003 <m.schaeferpham@gmail.com>
2021-12-02 18:27:39 -0700

    First commit

---
+++ PDFShort.py
@@ -0,0 +1,71 @@
+import PyPDF2
+import os
+
+def findUseful(filename):
+    """
+    Finds pages that aren't redundant
+
+    Arguments:
+        filename (string): the name of the original pdf
+        reader (PDFFileReader): a PDFFileReader for the pdf to be modified
+
+    Return:
+        Returns a list of pages containing the pages that aren't redundant
+    """
+    fin = open("LongPDFs/" + filename,"rb")
+    reader = PyPDF2.PdfFileReader(fin)
+    pages = []
+    
+    for i in range(reader.numPages-1):
+        page = reader.getPage(i)
+        nextPage = reader.getPage(i+1)
+        if page.extractText() not in nextPage.extractText():
+            pages.append(page)
+
+    fin.close()
+    return pages
+
+def createNewPdf(filename,pages):
+    """
+    Creates a new pdf without any redundant pages in ShortPDFs
+
+    Arguments:
+        filename (string): the name of the original pdf
+        pages (list of pages): a list of non-redundant pages
+
+    Return:
+        None
+    """
+    fout = open("ShortPDFs/" + filename,"wb")
+    writer = PyPDF2.PdfFileWriter()
+    
+    for i in pages:
+        writer.addPage(i)
+    writer.write(fout)
+    fout.close()
+    
+
+def removeRedundant(filename):
+    """
+    Finds redundant pages, then creates a new pdf without those pages in
+    ShortPDFs
+
+    Arguments:
+        filename (string): the name of the original pdf
+
+    Return:
+        None
+    """
+    pages = findUseful(filename)
+    createNewPdf(filename,pages)
+
+
+def main():
+    files = os.listdir("LongPDFs")
+    for i in files:
+        if i[-4:] == ".pdf":
+            removeRedundant(i)
+    
+
+if __name__ == "__main__":
+    main()
---
+++ README.txt
@@ -0,0 +1,15 @@
+PDFShort
+A python program designed to remove redundant pages from pdfs, redundant pages being ones that are comrised
+entirely of text found in the next page.
+
+Contents:
+ - PDFShort.py: the program that shortens the pdfs
+ - LongPDFs: a directory containing the pdfs to be shortened
+ - ShortPDFs: a directory that the shortened pdfs are written to
+ - README - this file
+
+Running instructions:
+Place your pdf files in the LongPDFs folder, then run from the command line with python3 PDFShort.py
+
+WARNING: This program gets the pages right, but when copying pages to the new pdfs the formatting gets
+messed up for some reason
